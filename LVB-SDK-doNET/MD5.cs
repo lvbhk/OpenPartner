20bf43e2c2276551a75915e1e32f82abb105f804
guqingyu <448287886@qq.com>
2017-04-06 15:36:31 +0800

    add lvb.sdk.net

---
+++ LVB-SDK-doNET/MD5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVB_SDK_doNET
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MD5
+    {
+        /// <summary>
+        /// Gets the m d5.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns></returns>
+        public static String GetMD5(String str)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] bytValue, bytHash;
+            bytValue = System.Text.Encoding.UTF8.GetBytes(str);
+            bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+    }
+}
